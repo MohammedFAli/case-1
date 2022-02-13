e96e0107b880f48c1f7bd7fbd8ddcc9c2544d39b
MohammedFAli <71610948+MohammedFAli@users.noreply.github.com>
2022-02-13 12:22:29 -0500

    Update testfile.c

--- testfile.c
+++ testfile.c
@@ -1,1 +1,13 @@
+Array I[] = [1,2,3,4,5,6,7,8]
 
+enqueue I[0]
+
+i = 1
+while ( i < n) {
+  if (Intersect(I[i]) //if I[i] intersects front
+      Q.front
+      enqueue(I[i])
+  else
+      Q.front
+      Q.dequeue()
+      }
